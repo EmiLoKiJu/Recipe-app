485234ed53d66990db860d6fa16fb8247b17d41c
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-05 04:18:36 +0300

    Update Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,12 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  has_many :ingredients, dependent: :destroy
+  has_many :recipes, through: :ingredients
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
