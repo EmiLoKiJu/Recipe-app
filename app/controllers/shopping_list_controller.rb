c67b212f74e72a00001e88e6582d23ba1f47d9bc
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-09-06 09:53:29 -0800

    Add local storage to save the recipe shopping list successfully

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,8 +2,8 @@ class ShoppingListController < ApplicationController
   skip_load_and_authorize_resource only: :index
 
   def index
-    selected_recipe_id = localStorage['selectedRecipeId']
-
+    selected_recipe_id = params[:selected_recipe_id]
+    puts "Selected recipe ID: #{selected_recipe_id.inspect}"
     if selected_recipe_id.present?
       @recipe = Recipe.find(selected_recipe_id)
       @shopping_list_items = @recipe.foods
